d654234472e7297f418289d1034c2ae1048293d6
Krzysztof Kałamarski <kkalamarski@gmail.com>
2016-08-12 17:37:01 +0300

    Change DB  Schema

--- src/js/data/DB.js
+++ src/js/data/DB.js
@@ -2,7 +2,7 @@ import Dexie from 'dexie';
 
 const DB = new Dexie("Player");
 
-DB.version(1).stores({ history: "++id, played, *track_id, trackName, total, cover, artistName, albumName, stream_url, plays" });
+DB.version(1).stores({ history: "++id, played, &track_id, trackName, total, cover, artistName, albumName, stream_url, plays" });
 
 DB.open().catch(function (e) {
     console.error("Open failed: " + e);
